bad56053d29da10a1da56626ceb1f91011dcdfc0
TaruDhir2007 <tarudhir2007@gmail.com>
2020-11-26 22:34:12 +0530

    box number 7 is added

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var box6, box7;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -18,12 +18,12 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+    box6 = new Box(600, 400, 70, 70)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    box7 = new box1(100,200,200,200)
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -49,12 +49,12 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    box6.diaplay();
     box3.display();
     box4.display();
     pig3.display();
     log3.display();
-
+    box7.display();
     box5.display();
     log4.display();
     log5.display();
